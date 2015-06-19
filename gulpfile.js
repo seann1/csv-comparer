5ddded3f36ca45eb1081fe5a4be32a1964287b8f
Sean Niesen <seanniesen@gmail.com>
2015-06-19 16:13:57 -0700

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	gulp.src('./sass/**/*.scss')
+	    .pipe(sass().on('error', sass.logError))
+	    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  	gulp.watch('./sass/**/*.scss', ['sass']);
+});
 
 gulp.task('scripts', function() {
     return gulp.src(['bower_components/jquery/dist/jquery.js', 
@@ -15,7 +26,7 @@ gulp.task('scripts', function() {
 gulp.task('styles', function() {
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css',
     				'bower_components/bootstrap/dist/css/bootstrap-theme.css',
-    				'style.css'])
+    				'css/styles.css'])
       	.pipe(minifyCss({compatibility: 'ie8'}))
       	.pipe(concat('main.min.css'))
       	.pipe(gulp.dest('build/css'));
