ce1e5b8f2c1bb6c7748f4d2d0e358f3ab6d61aa3
Sean Niesen <seanniesen@gmail.com>
2015-06-19 15:45:32 -0700

    gulp easier than grunt

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 var gulp = require('gulp');
-
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('scripts', function() {
     return gulp.src(['bower_components/jquery/dist/jquery.js', 
     				'bower_components/bootstrap/dist/js/bootstrap.js',
     				'scripts/main.js'])
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('build/js'));
+    	.pipe(uglify())
+      	.pipe(concat('main.min.js'))
+      	.pipe(gulp.dest('build/js'));
 });
 
 gulp.task('styles', function() {
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css',
     				'bower_components/bootstrap/dist/css/bootstrap-theme.css',
     				'style.css'])
-      .pipe(concat('main.css'))
-      .pipe(gulp.dest('build/css'));
+      	.pipe(minifyCss({compatibility: 'ie8'}))
+      	.pipe(concat('main.min.css'))
+      	.pipe(gulp.dest('build/css'));
 });
 
 gulp.task('default', ['scripts', 'styles']);
