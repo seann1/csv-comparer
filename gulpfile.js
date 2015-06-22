21c15ea1f35a5fddc1be341db53f1d4c3950996b
Sean Niesen <seanniesen@gmail.com>
2015-06-21 22:05:09 -0700

    adding angular

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var connect = require('gulp-connect');
 
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('public/sass/**/*.scss')
 	    .pipe(sass().on('error', sass.logError))
-	    .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: true
-        }))
-	    .pipe(gulp.dest('css/'));
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+	    .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('public/sass/*.scss', ['sass']);
 });
 
 gulp.task('libraries', function() {
     return gulp.src(['bower_components/jquery/dist/jquery.js', 
     				'bower_components/bootstrap/dist/js/bootstrap.js',
-            'scripts/papaparse.min.js', 'scripts/highlight.pack.js', 'scripts/dropzone.js'])
+            'public/scripts/papaparse.min.js', 'public/scripts/highlight.pack.js', 'public/scripts/dropzone.js'])
     	.pipe(uglify())
       	.pipe(concat('libraries.min.js'))
       	.pipe(gulp.dest('build/js'));
@@ -35,13 +32,12 @@ gulp.task('libraries', function() {
 //       	.pipe(concat('main.min.js'))
 //       	.pipe(gulp.dest('build/js'));
 // });
-
 gulp.task('styles', function() {
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css',
-    				'bower_components/bootstrap/dist/css/bootstrap-theme.css', 'css/railscasts.css'])
+    				'bower_components/bootstrap/dist/css/bootstrap-theme.css', 'public/css/railscasts.css'])
       	.pipe(minifyCss({compatibility: 'ie8'}))
       	.pipe(concat('main.min.css'))
-      	.pipe(gulp.dest('build/css'));
+      	.pipe(gulp.dest('public/build/css'));
 });
 
 gulp.task('connect', function() {
@@ -49,13 +45,13 @@ gulp.task('connect', function() {
                     port: 8001 });
 });
 
-gulp.task('html', function () {
-  return gulp.src('.')
-        .pipe(connect.reload());
+gulp.task('reload', function () {
+  gulp.src(['public/scripts/main.js', 'public/index.html', 'public/css/*.css'])
+  .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  return gulp.watch(['./scripts/main.js', './css/*.css'], ['html']);
+  return gulp.watch(['public/scripts/main.js', 'public/css/*.css'], ['reload']);
 });
 
 gulp.task('default', ['libraries', 'styles', 'connect', 'watch', 'sass:watch']);
