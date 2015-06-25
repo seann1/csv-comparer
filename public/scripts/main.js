814dc75d5befd5a28fcb0f910b220b8fa1467925
Sean Niesen <seanniesen@gmail.com>
2015-06-24 23:42:08 -0700

    figuring out what to pass

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,50 +1,37 @@
 var csvParser = angular.module('csvParser', []);
 
-csvParser.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-            
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
+csvParser.service('fileUpload', function () {
+    this.parseFile = function(file) {
+        $(file).parse({
+             config: {
+                 header: true,
+                 complete: function(results, file) {
+                     var json = results.data;
+                     $('.content').fadeIn();
+                     readJson(json);
 
-csvParser.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file, uploadUrl){
-        var fd = new FormData();
-        fd.append('file', file);
-        $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-        .success(function(){
-        })
-        .error(function(){
-        });
+                     $('pre code').each(function(i, block) {
+                         hljs.highlightBlock(block);
+                     });
+                     $('#myModal').modal('hide');
+                 },
+                 error: function(err, file, inputElem, reason) { 
+                     console.log(err);
+                 }
+             }
+         });
     }
-}]);
+});
 
 csvParser.controller('csvCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
     
-    $scope.uploadFile = function() {
-        var file = $scope.myFile;
+    $scope.uploadFile = function(files) {
+        var file = files.item(0);
         console.log('file is ' + JSON.stringify(file));
-        var uploadUrl = "/fileUpload";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
+        fileUpload.parseFile(file);
     };
     
 }]);
-
-csvParser.controller('csvCtrl', function($scope) {
-
-});
 
 
 // $(document).ready(function() {
