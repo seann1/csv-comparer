8d6d764c1e7808c20f5810154b14ec5e3586b6ea
Sean Niesen <seanniesen@gmail.com>
2015-06-24 22:51:00 -0700

    add directive

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,49 @@
 var csvParser = angular.module('csvParser', []);
-csvParser.controller('csvCtrl', function($scope) {
-	$scope.csvContents = [];
-	$scope.csvFile = [];
-	$scope.addCsv = function(evt) {
-		var files = evt.dataTransfer.files
-        if (files.length > 0) {
-            scope.$apply(function(){
-                scope.files = []
-                for (var i = 0; i < files.length; i++) {
-                    scope.files.push(files[i])
-                }
-            })
+
+csvParser.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
         }
-	}
+    };
+}]);
+
+csvParser.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+}]);
+
+csvParser.controller('csvCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+    
+    $scope.uploadFile = function() {
+        var file = $scope.myFile;
+        console.log('file is ' + JSON.stringify(file));
+        var uploadUrl = "/fileUpload";
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+    
+}]);
+
+csvParser.controller('csvCtrl', function($scope) {
+
 });
 
 
