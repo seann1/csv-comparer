1475098f6359c541ad7784a29ef901a0433c5302
Sean Niesen <seanniesen@gmail.com>
2015-06-26 15:29:22 -0700

    working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,59 +1,41 @@
 var csvParser = angular.module('csvParser', []);
 
-csvParser.service('readJson', function() {
-    this.printJson = function(datum) {
-        return $scope.datums.push('&lt;datum code="'+datum.CODE+'">' + datum.DESCRIPTION + '&lt;/datum>' + '<br/>');
+csvParser.service('readJson', function() {}
+);
+
+csvParser.factory('fileUpload', ['readJson', '$q', function (file, $q) {
+    return {
+        parseFile: function(file) {}
     }
-});
 
-csvParser.service('fileUpload', ['readJson', '$q', function (file, $q) {
-    var deferred = $q.defer();
-    this.parseFile = function(file) {
+    }]);
 
-                     return $q(function(resolve, reject) {}
-                    Papa.parse(file, {
-                                    header: true,
-                                    complete: function(results, file) {
-                                        console.log(results);
-                                        var json = results.data;
-                                        $('.content').fadeIn();
-
-                                        $('pre code').each(function(i, block) {
-                                            hljs.highlightBlock(block);
-                                        });
-                                    $('#myModal').modal('hide');
-                                    var datums = [];
-                        
-                                    datums.push(_.map(results.data, function(datum){return datums.push('&lt;datum code="'+datum.CODE+'">' + datum.DESCRIPTION + '&lt;/datum>' + '<br/>')}));
-
-
-                                    return deffered.resolve(datums);
-
-                                     // return $scope.datums = datums;
-                                },
-                                error: function(err, file, inputElem, reason) { 
-                                    console.log(err);
-                                    return deferred.promise;
-                                }
-                        });
-                    }
-}]);
-
-csvParser.controller('csvCtrl', ['$scope', 'fileUpload', '$q', function($scope, fileUpload, $q){
+csvParser.controller('csvCtrl', ['$scope', 'fileUpload', 'readJson', function($scope, $q, fileUpload){
+    $scope.datums = [1, 2, 3];
     $scope.uploadFile = function(files) {
         var file = files.files[0];
-        fileUpload.parseFile(file).then(function(datum) {
-            return $scope.parent.datums = datum;
-        })
+        Papa.parse(file, {header: true, 
+                            complete: function(results, file) {
+                                function printJson(file) {
+                                    var list = [];
+                                    _.map(file, function(datum) {
+                                        return list.push('<datum code="'+datum.CODE+'">' + datum.DESCRIPTION + '</datum>' + '<br/>');
+                                    });
+                                    return list;
+                                }
+                                console.log(results.data);
+
+                                $scope.$apply(function() {
+                                    $scope.datums = printJson(results.data);
+                                });
+                                $('pre code').each(function(i, block) {
+                                    hljs.highlightBlock(block);
+                                });
+                            }
+                        });
     };
     
 }]);
-
-// function readJson(inputJson) {
-// 	$.each(inputJson, function(index, value) {
-// 		$('pre code').append('&lt;datum code="'+value.CODE+'">' + value.DESCRIPTION + '&lt;/datum>' + '<br/>');
-// 	});
-// }
 
 
 
