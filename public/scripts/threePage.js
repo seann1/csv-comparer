7d9ef122ab1c01e8c4e14a1a416769a170b9e781
Sean Niesen <seanniesen@gmail.com>
2015-06-30 23:04:34 -0700

    add texture

--- public/scripts/threePage.js
+++ public/scripts/threePage.js
@@ -11,11 +11,36 @@ function init() {
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    createSphere = function(radius, segments, rings) {
+       var material, sphere;
+       if (radius == null) {
+         radius = 50;
+       }
+       if (segments == null) {
+         segments = 16;
+       }
+       if (rings == null) {
+         rings = 16;
+       }
+       sphere = new THREE.SphereGeometry(radius, segments, rings);
+       material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('assets/iris.gif') } );
+       return new THREE.Mesh(sphere, material);
+   };
+
+    geometry = new THREE.TorusGeometry( 200, 40, 16, 100 );
+    material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('assets/iris.gif') } );
 
     mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    scene.add(mesh);
+    light = new THREE.PointLight(0x0040ff, 20, 100 );
+    light.position.x = 20;
+    light.position.y = 10;
+    light.position.z = 3;
+    light.intensity = 100;
+    object = createSphere();
+    scene.add(new THREE.AmbientLight(0xff00F0));
+    scene.add(light);
+    scene.add(object);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( 170, 160 );
@@ -25,6 +50,10 @@ function init() {
 }
 
 function animate() {
+    var time;
+    time = new Date().getTime() * 0.0005;
+    light.position.x = Math.sin(time * 0.7) * 30;
+    object.rotation.x += 0.02;
 
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
