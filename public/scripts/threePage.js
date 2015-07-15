286928b47c6b328021dad803ff32ad7732c3339d
Sean Niesen <seanniesen@gmail.com>
2015-07-14 22:16:38 -0700

    animate light color

--- public/scripts/threePage.js
+++ public/scripts/threePage.js
@@ -42,7 +42,8 @@ function init() {
          rings = 16;
        }
        sphere = new THREE.SphereGeometry(radius, segments, rings);
-       material = new THREE.MeshPhongMaterial({specular: '#a9fcff', emissive: '#006063', shininess: 500});
+       material = new THREE.MeshPhongMaterial({specular: '#a9fcff', emissive: '#006063', shininess: 500, transparent: true});
+       material.opacity = 0.6;
        return new THREE.Mesh(sphere, material);
    };
 
@@ -106,21 +107,20 @@ function init() {
     for(var i = 0; i < 14; i++) {
       colorsArray = [0x00ff65, 0xe1ff00, 0xfa00ff, 0x0040ff, 0x00ff04, 0xffaa00];
       lights[i] = new THREE.PointLight(_.sample(colorsArray), 1, 100 );
-      lights[i].position.set(_.random(2, 300), _.random(2, 300), _.random(2, 100));
+      lights[i].position.set(_.random(20, 60), _.random(100, 200), _.random(-20, 20));
       lights[i].intensity = _.random(2, 10);
       scene.add(lights[i]);
-      x -= 10;
-      y -= 10;
-      z -= 10;
+      x -= 50;
+      y -= 50;
+      z -= 50;
     }
     var radius = 100;
     object = createSphere();
     otherObject = createOtherSphere();
-    scene.add(new THREE.AmbientLight(0xff00F0));
+    var ambLight = new THREE.AmbientLight(0xff00F0);
+    scene.add(ambLight);
     scene.add(otherObject);
     scene.add(object);
-    console.log(otherObject.geometry);
-    console.log(object.geometry.faces);
 
     renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize( window.innerWidth, (window.innerWidth / 1.5) );
@@ -170,6 +170,7 @@ function init() {
     sphereTween.start();
 
     document.getElementById("threed").appendChild( renderer.domElement );
+    console.log(scene);
 }
 
 function animate() {
